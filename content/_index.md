c67873dc54abc0eedf44938bea19359d6d09ef08
Mia Winter <mia@miawinter.de>
2024-04-22 01:15:39 +0200

    overhauled website design and content (wip)

--- content/_index.md
+++
@@ -1,9 +0,0 @@
----
-title: "Homepage"
-date: 2022-11-20T13:02:09+01:00
-draft: false
----
-
-Hello and Welcome to my Website!
-
-Here you gonna find my Projects that I'm proud of, some of my skills and also my blog!
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,47 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 module.exports = {
-    content: ["./content/**/*.{html,js}", "./layouts/**/*.{html,js}"],
+    content: ["index.html"],
     theme: {
         extend: {
-            height: {
+            colors: {
+                primary: 'rgb(var(--color-primary) / <alpha-value>)',
+                'primary-content': 'rgb(var(--color-primary-content) / <alpha-value>)',
+                secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
+                'secondary-content': 'rgb(var(--color-secondary-content) / <alpha-value>)',
+                neutral: 'rgb(var(--color-neutral) / <alpha-value>)',
+                'neutral-content': 'rgb(var(--color-neutral-content) / <alpha-value>)',
+
+                base: {
+                    100: 'rgb(var(--color-base-100) / <alpha-value>)',
+                    200: 'rgb(var(--color-base-200) / <alpha-value>)',
+                    300: 'rgb(var(--color-base-300) / <alpha-value>)',
+                },
+                'base-content': 'rgb(var(--color-base-content) / <alpha-value>)',
+            },
+            fontSize: {
+                sm: '0.750rem',
+                base: '1rem',
+                lg: '1.1rem',
+                xl: '1.333rem',
+                '2xl': '1.777rem',
+                '3xl': '2.369rem',
+                '4xl': '3.158rem',
+                '5xl': '4.210rem'
+            },
+            spacing: {
                 '128': '32rem',
+            },
+            boxShadow: {
+                'block': '4px 4px 0 0 #020202'
             }
         },
+        fontFamily: {
+            sans: ['Josefin Sans', ...defaultTheme.fontFamily.sans]
+        }
     },
-    plugins: [require("@tailwindcss/typography"), require("daisyui")],
+    plugins: [],
     daisyui: {
         logs: false,
         themes: [
