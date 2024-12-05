ec211cd1ac2f1bd28c15544d074e591d2f64457d
Mia Rose Winter <mia@miawinter.de>
2024-12-05 15:18:42 +0100

    Optimized Images (even more)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(
-      {
-        applyBaseStyles: false
-      }
-  )]
+    integrations: [tailwind(
+        {
+            applyBaseStyles: false
+        }
+    )],
+    image: {
+        remotePatterns: [{protocol: "https"}],
+        domains: ["miawinter.de", "winter-software.com", "geekslist.de", "tech.lgbt", "matrix.org", "ko-fi.com"]
+    }
 });
