1ab3eb63692f2b91d632ccb9f764b2012be3f1b3
Mia Rose Winter <mia@miawinter.de>
2025-12-16 12:11:16 +0100

    migrated to astro 5 and tailwind 4

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-import defaultTheme from 'tailwindcss/defaultTheme'
-import type { Config } from "tailwindcss";
 
-const config: Config = {
-	darkMode: ['selector', '[data-theme="dark"]'],
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+const config = {
     theme: {
         extend: {
             colors: {
@@ -21,30 +17,7 @@ const config: Config = {
                     300: 'rgb(var(--color-base-300) / <alpha-value>)',
                 },
                 'base-content': 'rgb(var(--color-base-content) / <alpha-value>)',
-            },
-            fontSize: {
-                sm: '0.750rem',
-                base: '1rem',
-                lg: '1.1rem',
-                xl: '1.333rem',
-                '2xl': '1.777rem',
-                '3xl': '2.369rem',
-                '4xl': '3.158rem',
-                '5xl': '4.210rem'
-            },
-            spacing: {
-                '128': '32rem',
-            },
-            boxShadow: {
-                'block': '4px 4px 0 0 #020202'
             }
-        },
-        fontFamily: {
-            sans: ["Josefin Sans", ...defaultTheme.fontFamily.sans],
-            old: ["serif"]
         }
-    },
-    plugins: [],
+    }
 }
-
-export default config;
